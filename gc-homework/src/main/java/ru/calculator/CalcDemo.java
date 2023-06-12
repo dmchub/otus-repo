a893546b6c5f283363b807d17c6e2e561c00d768
Anton Sokolov <Anton_Sokolov@epam.com>
2023-06-12 14:20:47 +0300

    Done

--- gc-homework/src/main/java/ru/calculator/CalcDemo.java
+++ gc-homework/src/main/java/ru/calculator/CalcDemo.java
@@ -5,7 +5,7 @@ package ru.calculator;
 -Xms256m
 -Xmx256m
 -XX:+HeapDumpOnOutOfMemoryError
--XX:HeapDumpPath=./logs/heapdump.hprof
+-XX:HeapDumpPath=C:\project\otus\gc-homework\logs\heapdump.hprof
 -XX:+UseG1GC
 -Xlog:gc=debug:file=./logs/gc-%p-%t.log:tags,uptime,time,level:filecount=5,filesize=10m
 */
